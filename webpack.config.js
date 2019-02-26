a4250523d6b2d404c3836962ad6f1809c6f99f5a
joyme <jiangpengfei12@gmail.com>
2019-02-26 13:09:21 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    devtool: "inline-source-map",
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    node: {
+        fs: "empty"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /fest\.json$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: __dirname + '/src/assets',
+            to: __dirname + '/dist/assets'
+        }, {
+            from: __dirname + '/src/manifest.json',
+            to: __dirname + '/dist/manifest.json'
+        }]),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: require('html-webpack-template'),
+            title: '我爱学习',
+            bodyHtmlSnippet: '<img id="img" src="assets/icon.jpg">',
+        })
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
