1aa6cc8fbeb96e42069fd2727fb1c594b9499c19
jiangpengfei <jiangpengfei12@gmail.com>
2019-02-26 19:48:38 +0800

    可以使用

--- src/background.ts
+++ src/background.ts
@@ -11,36 +11,60 @@ nsfwjs.load('/assets/model/').then(model => {
 
 // let imgs: any;
 
+function sendImgClassifyResult(result, cursor) {
+
+    let block = {}
+    block['Porn'] = true;
+    block['Sexy'] = true;
+    block['Hentai'] = true;
+    block['Neutral'] = true;
+    block['Drawing'] = true;
+
+    if (block[result[0].className]){
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "classify", block: cursor }, function (response) {
+            })
+        })
+    }
+
+}
+
 // 监听快捷键
 chrome.commands.onCommand.addListener(function (command) {
-    console.log(command)
-    alert("快捷键")
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "match_images" }, function (response) {
             if (response.action == "classify") {
                 // Classify the image
                 console.log("收到imgSrc", response.imgSrc)
-                const imgs = JSON.parse(response.imgSrc)
+                const imgs = response.imgSrc
+
+                let cursor = 0
 
                 imgs.forEach(imgSrc => {
-                    let canvas = document.createElement("canvas");
-                    const context = canvas.getContext("2d");
-                    let img = new Image();
-                    img.addEventListener("load", function () {
-                        canvas.width = img.width;
-                        canvas.height = img.height;
-                        context.drawImage(img, 0, 0);
-                        const imgData = context.getImageData(0, 0, img.width, img.height);
-                        if (globalModel != null) {
-                            const predictions = globalModel.classify(imgData)
-                            predictions.then(res => {
-                                console.log("预测结果", res)
 
-                                // TODO: 这里拿到识别结果后，再反馈给content_script去屏蔽图片
-                            })
-                        }
-                    });
-                    img.src = imgSrc
+                    // 自执行函数，形成局部作用域
+                    (function(cursor, imgSrc, globalModel){
+                        let canvas = document.createElement("canvas");
+                        const context = canvas.getContext("2d");
+                        let img = new Image();
+                        img.addEventListener("load", function () {
+                            canvas.width = img.width;
+                            canvas.height = img.height;
+                            context.drawImage(img, 0, 0);
+                            const imgData = context.getImageData(0, 0, img.width, img.height);
+                            if (globalModel != null) {
+                                const predictions = globalModel.classify(imgData)
+                                predictions.then(res => {
+                                    console.log("预测结果", res)
+    
+                                    // TODO: 这里拿到识别结果后，再反馈给content_script去屏蔽图片
+                                    sendImgClassifyResult(res, cursor)
+                                })
+                            }
+                        });
+                        img.src = imgSrc
+                    }(cursor, imgSrc, globalModel))
+                    cursor++
                 })
 
             }
--- src/content_script.js
+++ src/content_script.js
@@ -2,22 +2,24 @@ import { array } from "@tensorflow/tfjs-data";
 
 // background 提供后台常驻的server，由contentScript将图片发送给background.js来处理
 
-alert("content script 加载")
+var globalImgElements
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log("content_script收到消息", request);
         if (request.action == "match_images") {
-            const imgs = document.getElementsByTagName("img");
+            globalImgElements = document.getElementsByTagName("img");
 
             let imgSrcs = []
 
-            Array.from(imgs).forEach(img => {
-                console.log(img)
+            Array.from(globalImgElements).forEach(img => {
                 imgSrcs.push(img.src)
             })
 
-            sendResponse({action: 'classify', imgSrc: JSON.stringify(imgSrcs)});
+            sendResponse({action: 'classify', imgSrc: imgSrcs});
+        } else if (request.action == "classify") {
+            console.log("收到屏蔽index", request.block)
+            globalImgElements.item(request.block).src = chrome.extension.getURL('assets/icon.jpg');;
         }
     }
 );
